a9ffbbc72b0561316c8c823440399cb6790ec2ce
DMurashko <appleitunesstore590@gmail.com>
2021-03-04 16:43:43 +0200

    Improved responsive design

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
 import Button from '@material-ui/core/Button';
-import './Main.scss';
+import Styles from './Main.module.scss';
 
 function Main(params) {
 	return (
-		<main>
+		<main className={Styles.main}>
 			<Link to="/characters">
 				<Button
 					variant="outlined" 
